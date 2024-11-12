db40d4f83e20883f31aaf44b2bf9c24786e5798c
Danil gdj <danilguidjou7@gmail.com>
2024-11-12 19:23:41 +0100

    pi + archi

--- Tp-Programmation-Imperative/TP6/TP6exo2/main.c
+++ Tp-Programmation-Imperative/TP6/TP6exo2/main.c
@@ -1,66 +1,62 @@
 /*
  * main.c
  *
- *  Created on: 23 oct. 2024
- *      Author: iq22282
+ *  Created on: 11 nov. 2024
+ *      Author: danil
  */
 
-#include <stdio.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
+int retrouvePosition(char tab1[], char lettre){
+	for(int i=0; i<7; i++){
+		if(lettre == tab1[i]){
+			return i;
+		}
+	}
+	return -1;
+}
 
-int  valeurChiffRom(char *chiffRom, int tab[]){
-	int somme = 0;
-	int i=0;
-	for(int i=0; i< strlen(chiffRom); i++){
-		if(chiffRom[i]>= 'a' && chiffRom[i] <= 'z'){
-			chiffRom[i] = chiffRom[i] + ('A' -'a');
+int decrypte(char tabRom[] , int tabVal[], char chifRom[]){
+	int taille = strlen(chifRom);
+	int valChif = 0;
+	int temp;
+	int temp2;
+
+	for(int i =0; i<taille ; i++){
+		if(i == taille - 1 ){
+			temp = retrouvePosition(tabRom, chifRom[i]);
+			if(temp != -1){
+				valChif += tabVal[temp];
+			}else{
+				break;
+			}
+
+		}else{
+			temp = retrouvePosition(tabRom, chifRom[i]);
+			temp2 = retrouvePosition(tabRom, chifRom[i+1]);
+			if(tabVal[temp] > tabVal[temp2]){
+				valChif += tabVal[temp];
+			}else{
+				valChif -= tabVal[temp];
+			}
 		}
 	}
 
-	while(chiffRom[i] != '\0' && somme != -1){
-		switch(chiffRom[i]){
-		case 'I':
-			somme += tab[1];
-			break;
-		case 'V':
-			somme+= tab[2];
-			break;
-		case 'X':
-			somme += tab[3];
-			break;
-		case 'L':
-			somme += tab[4];
-			break;
-		case 'C':
-			somme += tab[5];
-			break;
 
-		case 'D':
-			somme += tab[7];
-			break;
-		case 'M':
-			somme += tab[8];
-			break;
-		default:
-			somme = -1;
-		}
-		i++;
-	}
+	return valChif;
 }
 
 int main(){
-	int tabRef[] = {1,5,10,50,100,500,1000};
-	char chiffRom[15];
-	int valeur;
-	printf("entrez un chiffre romain : ");
+	char tabChifRom[] ={'I', 'V' , 'X' , 'L' , 'C', 'D' , 'M'};
+	int tabValChif[] = {1, 5, 10, 50, 100, 500, 1000};
+	char chiffRom[30];
+	printf("entrez une chiffre romain : ");
 	scanf("%s", chiffRom);
-	valeur = valeurChiffRom(chiffRom, tabRef);
-	if(valeur != -1){
-		printf("la valeur du chiffre romain %s : %d", chiffRom, valeur);
-	}else{
-		printf("erreur");
-	}
+	printf("le chiffre romain que vous avez saisie est : %s\n", chiffRom);
+
+	printf("la valeur du chiffre est : %d", decrypte(tabChifRom, tabValChif, chiffRom));
 	return EXIT_SUCCESS;
 }
