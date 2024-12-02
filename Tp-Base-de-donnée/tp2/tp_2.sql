e5532569bcc75258ef63ed90d45c618b7ae66328
Danil gdj <danilguidjou7@gmail.com>
2024-12-02 19:53:17 +0100

    TP2 psql bdd

--- Tp-Base-de-donnée/tp2/tp_2.sql
+++ Tp-Base-de-donnée/tp2/tp_2.sql
@@ -125,12 +125,130 @@ FROM joueur J , gain G
 WHERE J.nom = G.nomJoueur 
         AND G.date = 1985
         AND G.lieuTournoi = 'Roland Garros'
-        AND  EXISTS (SELECT J.nom 
-                      FROM joueur J , gain G
-                      WHERE J.nom = G.nomJoueur 
+        AND  EXISTS (SELECT j.nom 
+                      FROM joueur j , gain G
+                      WHERE j.nom = J.nom
+                      AND j.nom = G.nomJoueur 
                       AND G.date = 1985
                       AND G.lieuTournoi = 'Wimbledon ');
 
 
 -- i -- 
------ 1. ----
+----- 1. JUSTE AVEC EXCEPT ----
+
+SELECT nomJoueur
+FROM gain
+WHERE lieuTournoi = 'Roland Garros'
+EXCEPT 
+        SELECT nomJoueur
+        FROM gain
+        WHERE prime < 1000000
+        AND lieuTournoi = 'Roland Garros';
+
+----- 2. SANS EXCEPT ----
+SELECT nomJoueur
+FROM gain
+WHERE lieuTournoi = 'Roland Garros'
+        AND nomJoueur NOT IN ( SELECT nomJoueur
+                                FROM gain
+                                WHERE prime < 1000000
+                                AND lieuTournoi = 'Roland Garros');
+
+-- m --
+----- 1.AVEC INTERSECT ET EXCEPT ----
+
+(SELECT nomGagnant
+FROM rencontre
+WHERE lieuTournoi = 'Roland Garros'
+EXCEPT  SELECT nomPerdant
+        FROM rencontre 
+        WHERE lieuTournoi = 'Roland Garros')
+INTERSECT
+        (SELECT nomPerdant
+        FROM rencontre 
+        WHERE lieuTournoi = 'Wimbledon'
+        EXCEPT SELECT nomGagnant
+                FROM rencontre
+                WHERE lieuTournoi = 'Wimbledon');
+
+----- 2.AVEC IN et EXISTS ----
+
+SELECT DISTINCT nomGagnant 
+FROM rencontre
+WHERE lieuTournoi = 'Roland Garros'
+        AND nomGagnant NOT IN (SELECT DISTINCT nomPerdant 
+                                FROM rencontre
+                                WHERE lieuTournoi = 'Roland Garros')
+        AND EXISTS (SELECT DISTINCT nomPerdant FROM rencontre
+                                WHERE lieuTournoi = 'Wimbledon'
+                                        AND nomPerdant NOT IN (SELECT DISTINCT nomGagnant FROM rencontre
+                                                                WHERE lieuTournoi = 'Wimbledon'));
+
+-- n --
+
+----- 1.AVEC EXCEPT ----
+-- probleme
+SELECT R.nomGagnant , R.nomPerdant
+FROM rencontre R, joueur J1, joueur J2
+WHERE R.nomGagnant = J1.nom AND R.nomGagnant = J1.prenom
+        AND R.nomPerdant = J2.nom AND R.nomGagnant = J2.prenom
+        AND NOT EXISTS (SELECT *
+                    FROM rencontre r1
+                    WHERE R.nomGagnant = r1.nomPerdant)
+        AND NOT EXISTS ( SELECT *
+                        FROM rencontre r2
+                        WHERE R.nomPerdant = r2.nomGagnant);
+
+-- o --
+SELECT 
+
+-- p --
+SELECT count(*)
+FROM gain
+WHERE lieuTournoi = 'Wimbledon'
+        AND date = 1989;
+
+-- q --
+SELECT gain.date , avg(gain.prime)
+FROM gain
+GROUP BY gain.date;
+
+-- r --
+SELECT count() AS numero, J.nom , J.prenom
+FROM joueur J;
+
+-- s --
+INSERT INTO joueur (nom, prenom, nationalite)
+VALUES ('Guidjou' , 'Danil' , 'Algerie');
+
+-- t -- 
+UPDATE joueur 
+SET anneeNaissance = 2004
+WHERE nom = 'Guidjou';
+
+-- u -- 
+DELETE FROM joueur 
+WHERE nom = 'Guidjou';
+
+-- v -- 
+DELETE FROM joueur
+WHERE nom = 'Noah';  --erreur car le tuple ou nom = 'Noah' est une cle etrangere pour un tuple dans la table gain
+
+-- w -- ; erreur
+UPDATE rencontre
+SET 
+DELETE FROM gain 
+WHERE nomJoueur = 'Noah';
+
+DELETE FROM joueur
+WHERE nom = 'Noah';
+
+-- X --  : erreur
+SELECT G.nomJoueur , G.prime
+FROM gain G
+WHERE 200000 > ALL (SELECT sum(prime) 
+                    FROM gain
+                    WHERE G.nomJoueur = nomJoueur)
+GROUP BY nomJoueur , prime;
+
+-- 
